0f0305f55bd9d69bb9e7da7595f306ac417222da
Noah Solnick <soltrain@berkeley.edu>
2016-02-15 23:56:44 -0500

    grabbing values

--- app.py
+++ app.py
@@ -2,21 +2,23 @@ from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-
+@app.route("/")
 def main():
 	return render_template('index.html')
 
+@app.route('/process', methods = ['POST'])
 def process():
 	#read values
 	_value1 = request.form['value1']
 	_value2 = request.form['value2']
-
+	print("First value is'" + _value1 + "'")
+	print("Second value is'" + _value2 + "'")
+	return redirect('/')
 	#check values
-	if _value1 and _value2:
-		return json.dumps({'html':'<span>All fields received</span'})
-	else
-		return json.dumps({'html':'<span>Required fields missing</span>'})
+	#if _value1 and _value2:
+	#	return json.dumps({'html':'<span>All fields received</span'})
+	#else
+	#	return json.dumps({'html':'<span>Required fields missing</span>'})
 
 if __name__ == "__main__":
 	app.run(debug=True)
