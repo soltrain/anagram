92c27f60a030ab715ffdf810a6844beee27a7a1d
Noah Solnick <soltrain@berkeley.edu>
2016-02-16 00:28:19 -0500

    finished

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, redirect
 
 app = Flask(__name__)
 
@@ -11,13 +11,18 @@ def process():
 	#read values
 	_value1 = request.form['value1']
 	_value2 = request.form['value2']
-	print("First value is'" + _value1 + "'")
-	print("Second value is'" + _value2 + "'")
-	return redirect('/')
-	#check values
+	#print("First value is'" + _value1 + "'")
+	#print("Second value is'" + _value2 + "'")
+	if _value1[::-1] == _value2:
+		return render_template('index.html', result = 'Yes, indeed it is.')
+	else:
+		return render_template('index.html', result = 'No, it is not.')
+	#return redirect('/')
+	
+	#check values / not gonna use javascript afterall
 	#if _value1 and _value2:
 	#	return json.dumps({'html':'<span>All fields received</span'})
-	#else
+	#else:
 	#	return json.dumps({'html':'<span>Required fields missing</span>'})
 
 if __name__ == "__main__":
