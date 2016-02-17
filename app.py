79a00416b69ebedc153746b6759c90de2e53e4d1
Noah Solnick <soltrain@berkeley.edu>
2016-02-17 15:35:04 -0500

    fixed logic, palindromes !=anagrams

--- app.py
+++ app.py
@@ -7,25 +7,50 @@ app = Flask(__name__)
 def main():
 	return render_template('index.html')
 
+def calcAnagram(var1, var2):
+	x = 0
+	i = 0
+	match = True 
+	var1 = var1.lower()
+	var1 = sorted(var1)
+	var2 = var2.lower()
+	var2 = sorted(var2)
+	#print 'sorted var1:', var1
+	#print 'sorted var2:', var2
+
+	if len(var1) == len(var2):
+		for x in var1:
+			if var1[i] == var2[i]: 
+				i = i + 1
+			else:
+				match = False
+				break
+	else:
+		match = False
+
+	if match == False:
+		return 0
+	else:
+		return 1
+
 @app.route('/process', methods = ['POST'])
 def process():
 	#read values
-	_value1 = request.form['value1']
-	_value2 = request.form['value2']
-	#print("First value is'" + _value1 + "'")
-	#print("Second value is'" + _value2 + "'")
-	if _value1[::-1] == _value2:
-		return render_template('index.html', result = 'Yes, indeed it is.')
-	else:
-		return render_template('index.html', result = 'No, it is not.')
-	#return redirect('/')
+	value1 = request.form['value1']
+	value2 = request.form['value2']
 	
-	#check values / not gonna use javascript afterall
-	#if _value1 and _value2:
-	#	return json.dumps({'html':'<span>All fields received</span'})
-	#else:
-	#	return json.dumps({'html':'<span>Required fields missing</span>'})
-
+	#print("First value is'" + value1 + "'")
+	#print("Second value is'" + value2 + "'")
+	#print(calcAnagram(value1,value2))
+	
+	if calcAnagram(value1,value2) == 0:
+		return render_template('index.html', result = 'No, it is not.')
+	else:
+		return render_template('index.html', result = 'Yes, indeed it is.')
+	
+	
 if __name__ == "__main__":
+	#need this for heroku 
 	port = int(os.environ.get("PORT", 5000))
+	#app.debug = True
 	app.run(host='0.0.0.0', port=port)
